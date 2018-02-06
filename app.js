7f968d040a5630a51101d82f0c1be0c8c43afcb6
Gustav Westberg <31844570+Gustavwes@users.noreply.github.com>
2018-02-06 13:57:53 +0100

    Added basic functionality of navbar
    Navbar click (omgivning)

--- app.js
+++ app.js
@@ -8,5 +8,8 @@ app.listen(6662);
 app.get('/1', function(req, res){
     res.sendFile(__dirname + '/public/test.html');
 });
+app.get('/Omgivning', function(req, res){
+    res.sendFile(__dirname + '/public/Omgivning.html');
+});
 
 console.log("Lyssnar på port 6662");
--- public/connection.js
+++ public/connection.js
@@ -2,3 +2,11 @@ $.get('/1', function(response){
     console.log("21314raf");
     document.getElementById("mainContent").innerHTML = response;
 });
+
+function Omgivning(){
+    $.get('/Omgivning', function(response){
+        
+        document.getElementById("mainContent").innerHTML = response;
+    });
+
+}
